c8308be0f73e0da1b18972c5972882d8793b7f5d
velizarmitov <v.mitov9898@gmail.com>
2025-12-04 20:00:53 +0200

    refactor GlobalExceptionHandler

--- src/main/java/com/phitrading/pricing/web/controller/GlobalExceptionHandler.java
+++ src/main/java/com/phitrading/pricing/web/controller/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.phitrading.pricing.web.controller;
 
 import com.phitrading.pricing.common.exception.InstrumentNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(InstrumentNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleInstrumentNotFound(InstrumentNotFoundException ex) {
