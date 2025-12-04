59af1343fd77b925225ce76a3c6077b278c6b4a5
velizarmitov <v.mitov9898@gmail.com>
2025-12-04 19:03:37 +0200

    refactor PriceSimulationScheduler

--- src/main/java/com/phitrading/pricing/domain/service/scheduler/PriceSimulationScheduler.java
+++ src/main/java/com/phitrading/pricing/domain/service/scheduler/PriceSimulationScheduler.java
@@ -2,8 +2,8 @@ package com.phitrading.pricing.domain.service.scheduler;
 
 import com.phitrading.pricing.domain.entity.InstrumentPrice;
 import com.phitrading.pricing.domain.repository.InstrumentPriceRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,11 +14,15 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
-@RequiredArgsConstructor
-@Slf4j
 public class PriceSimulationScheduler {
 
+    private static final Logger log = LoggerFactory.getLogger(PriceSimulationScheduler.class);
+
     private final InstrumentPriceRepository repository;
+
+    public PriceSimulationScheduler(InstrumentPriceRepository repository) {
+        this.repository = repository;
+    }
 
     @Scheduled(cron = "0 */1 * * * *")
     @CacheEvict(cacheNames = "instrumentPrices", allEntries = true)
